f68f52d568504f2a3a5ef26715586b3c1a60a707
John Adams <john.adams@topper.wku.edu>
2017-07-24 17:17:13 -0400

    added all teams

--- API.js
+++ API.js
@@ -1,14 +1,58 @@
+function makeApiCall(url) {
+  $.ajax({
+    type:"GET",
+    dataType: 'json',
+    url: url,
+    success: function(data) {
+      console.log(data);
+
+      $('.text').text(JSON.stringify(data));
+    },
+    fail: function (err){console.log(err)}
+
+  });
+
+
+}
+
+const teams = {'cardinals': 'ARI', 'falcons': 'ATL', 'ravens': 'BAL', 'bills': 'BUF', 'panthers': 'CAR', 'bears': 'CHI', 'bengals': 'CIN', 'browns': 'CLE', 'cowboys': 'DAL', 'broncos': 'DEN', 'lions': 'DET', 'packers': 'GB', 'texans': 'HOU', 'colts': 'IND', 'jaguars': 'JAC', 'chiefs': 'KC', 'chargers': 'LAC', 'rams': 'LA', 'dolphins': 'MIA', 'vikings': 'MIN', 'patriots': 'NE', 'saints': 'NO',  'giants': 'NYG', 'jets': 'NYJ', 'raiders': 'OAK', 'eagles': 'PHI', 'steelers': 'PIT', '49ers': 'SF', 'seahawks': 'SEA', 'buccaneers': 'TB', 'titans': 'TEN', 'redskins': 'WAS'};
+
+$('img').click(function() {
+
+let team =  $(this).attr('data-team');
+  $('.text').text('loading . . .');
+console.log(team, teams[team]);
+let url =  `http://nflarrest.com/api/v1/team/timeline/${teams[team]}`;
+
+  makeApiCall(url)
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $('.btn').click(function() {
 
   $('.text').text('loading . . .');
 
-  $.ajax({
-    type:"GET",
-    url:"http://nflarrest.com/api/v1/",
-    success: function(data) {
-      $('.text').text(JSON.stringify(data));
-    },
-    dataType: 'jsonp',
-  });
+let url =  "http://nflarrest.com/api/v1/crime"
+
+  makeApiCall(url)
+
+
 
 });
